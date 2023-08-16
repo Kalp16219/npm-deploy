a58af88e759e93d4e7515b1f1fdc25719b172a54
Kalp16219 <134376074+Kalp16219@users.noreply.github.com>
2023-08-16 17:14:46 +0530

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 npm version patch
 version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/package.json | grep '^\+.*version' | sed -s 's/[^0-9\.]//g'`
+echo "version: "+version
 git add *;
+echo "Git Added "
 git commit -m "Commit message"
-
+echo "Commit Message Added "
 
 if [ "$version" != "" ]; then
-git tag -a "v$version" -m "`git log -1 --format=%s`"
-echo "Created a new tag, v$version"
-git push --tags
-npm publish
+    echo "In IF "
+    git tag -a "v$version" -m "`git log -1 --format=%s`"
+    echo "Git Tag added"
+    echo "Created a new tag, v$version"
+    git push --tags
+    echo "Git Tag Push"
+    npm publish
+    echo "NPM PUSH"
 fi
